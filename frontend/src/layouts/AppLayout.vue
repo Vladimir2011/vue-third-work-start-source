19e5c71a6f99752cb29629509a3b93c1a501a068
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2023-04-16 19:55:50 +0300

    add basic markup for project

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app_layout">
+    <app-layout-header/>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLayout.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
