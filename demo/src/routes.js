6180c3b0ff526dd91e1f671df19d495767b12668
Vladimir Garbuzov <vladimir.garbuzzov@yandex.ru>
2023-05-15 08:26:51 +0300

    work with demo

--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,7 +1,22 @@
 export default [
   {
     name: 'Counter Base',
-    path: '/counter-4',
-    component: () => import("./demo4/DemoCounter.vue"),
+    path: '/counter-0',
+    component: () => import('./demo0/DemoCounter.vue')
   },
-];
+  {
+    name: 'Counter Props & Events',
+    path: '/counter-1',
+    component: () => import('./demo1/DemoCounter.vue')
+  },
+  {
+    name: 'State management',
+    path: '/counter-4',
+    component: () => import('./demo4/DemoCounter.vue')
+  },
+  {
+    name: 'Server',
+    path: '/server',
+    component: () => import('./demo5/Server.vue')
+  }
+]
